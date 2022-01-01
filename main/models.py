532d1a986c6d56bccb8847510e178dad2d7c4eb9
sandakelum-dev <sandakelum.dev@gmail.com>
2022-01-01 15:44:02 +0530

    user register ok

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    user =models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    birthday = models.DateField()
+    placeOfBirth = models.CharField(max_length=200)
+    sex =models.CharField(max_length=10)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,64 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from main.models import Profile
+
+
+User = get_user_model()
+
+
+
+def register(req):
+    
+    context = {"errors":[],"msg":[]}
+    
+    if req.method == "POST":
+        #get post data to userData dict
+        userData = {
+            "name":req.POST['name'],
+            "bd":req.POST['bd'],
+            "pob":req.POST['pob'],
+            "sex":req.POST['sex'],
+            "email":req.POST['email'],
+            "password":req.POST['password']
+        }
+
+        #check if password is confirmed
+        if(req.POST["password"] == req.POST["re_password"]):
+            #check email already exists
+            e = User.objects.filter(email=userData['email'])
+            if e.exists() == False:
+                try:
+                    newUser = User()
+                    newUser.first_name = userData['name']
+                    newUser.email=userData['email']
+                    newUser.set_password(userData["password"])
+                    #set email to username
+                    newUser.username=userData['email']
+                    newUser.save()
+                    #set other data to profie model
+                    
+                    profile = Profile()
+                    profile.user = newUser
+                    profile.birthday = userData["bd"]
+                    profile.placeOfBirth = userData["pob"]
+                    profile.sex = userData["sex"]
+                    profile.save()
+                    
+                    context["msg"].append("User added!")
+                except Exception as e:
+                    print(e)#log error
+                    context["errors"].append("something wrong!")
+                    
+                    
+                
+            else:
+                context["errors"].append("This E-mail already exists!")
+        
+        pass
+        
+    
+    # DB.append()
+    
+    return render(req,"register.html",context)
